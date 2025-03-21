bed0f3c7107971d746c149035343889fbe05f0b3
abdukarim <kaumijabdukarim@gmail.com>
2025-03-21 15:36:45 +0500

    123

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 // alert('hello');
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".header__body-burger");
+    const nav = document.querySelector(".header__body-menu");
+
+    burger.addEventListener("click", function () {
+        this.classList.toggle("active");
+        nav.classList.toggle("active");
+    });
+
+    // Закрытие меню при клике вне его
+    document.addEventListener("click", function (e) {
+        if (!nav.contains(e.target) && !burger.contains(e.target)) {
+            nav.classList.remove("active");
+            burger.classList.remove("active");
+        }
+    });
+});
